e9fa1661938e4b3495d27e33464b3948a88dccc7
yarwen0 <nhoojahm@gmail.com>
2025-03-06 23:58:32 -0600

    Added Components and Navbar folder

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+    return (
+    <div className='flex min-h-screen w-full bg-gray-50 '>
+        {/* sidebar */}
+        sidebar
+        <main className={'flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'}>
+            {/* navbar */}
+            navbar
+            {children}
+
+        </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
