3266fe3f32c99c779e1c287d9b4857974428fca0
yarwen0 <nhoojahm@gmail.com>
2025-03-13 23:19:24 -0500

    Added search bar

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -8,7 +8,10 @@ const Navbar = () => {
     {/* Search Bar */}
     <div className='flex items-center gap-8'>
       <div className='relative flex h-min w-[200px]'>
-        <Search className='flex item-center'/>
+        <Search className='absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white'/>
+        <input 
+          className='w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white' type='search' placeholder='Search...'
+        />
       </div>
     </div>
   </div>
